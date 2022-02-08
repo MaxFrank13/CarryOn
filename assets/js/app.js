958b826199932191591842c7d295ea40af469c1a
dokodesara <dokodesara@gmail.com>
2022-02-07 19:19:30 -0500

    api request

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,12 +8,15 @@ $(function () {
     const state = $("#state");
     const cultural = $("#cultural");
     const foods = $("#foods");
-    const transport =$("#transport");
-   
+    const transport = $("#transport");
+    const bg = document.querySelector("body");
+    const imageKey = "Eaf6kScI2v8YQdUzjFbCdG3luvlmSVPCkR-ST2jKkIs";
 
     // **** Event Listeners ****
 
     searchBtn.click(handleSubmit);
+
+    getImage()
 
     // **** Functions ****
 
@@ -23,17 +26,17 @@ $(function () {
         const stateCode = state.val();
         let queryString = `./results.html?q=${searchVal},${stateCode},${countryCode}`
 
-        if (cultural[0].checked){ 
+        if (cultural[0].checked) {
             const culturalBox = cultural[0].value;
-           queryString +=  "&"+culturalBox;
+            queryString += "&" + culturalBox;
         }
-        if (foods[0].checked){ 
+        if (foods[0].checked) {
             const foodslBox = foods[0].value;
-           queryString +=  "&"+foodslBox;
+            queryString += "&" + foodslBox;
         }
-        if (transport[0].checked){ 
+        if (transport[0].checked) {
             const transportBox = transport[0].value;
-           queryString +=  "&"+transportBox;
+            queryString += "&" + transportBox;
         }
         location.assign(queryString);
         if (!searchVal) {
@@ -42,4 +45,17 @@ $(function () {
         }
 
     }
+    // **** Api request ****
+    function getImage() {
+        const requestUrl = `https://api.unsplash.com/search/photos/?query=travel&client_id=${imageKey}`;
+        fetch(requestUrl)
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(data){
+                console.log(data.results);
+                bg.style.backgroundImage = `url(${data.results[Math.floor(Math.random() * 10)].urls.regular}`;
+            })
+    }
 })
+
